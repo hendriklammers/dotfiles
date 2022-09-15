b1465f3ec9db56f0182267df901f2131eff45b7b
Hendrik Lammers <lammers@gmail.com>
2022-09-15 20:53:17 +0800

    Highlight yanked text

--- nvim/init.lua
+++ nvim/init.lua
@@ -446,3 +446,12 @@ vim.api.nvim_create_autocmd({ "WinLeave" }, {
 		vim.opt_local.cursorline = false
 	end,
 })
+
+-- highlight yanked text
+vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = "YankHighlight",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = "300" })
+	end,
+})
