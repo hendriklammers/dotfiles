c815764dbab6568ddd6a5a8abbfca18d4f83e087
Hendrik Lammers <lammers@gmail.com>
2023-02-21 07:11:59 +0700

    Use lua_ls instead of sumneko_lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,7 +5,7 @@ require("mason").setup()
 local servers = {
 	"rust_analyzer",
 	"tsserver",
-	"sumneko_lua",
+	"lua_ls",
 	"gopls",
 	"html",
 	"cssls",
@@ -56,7 +56,7 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require("lspconfig").sumneko_lua.setup({
+require("lspconfig").lua_ls.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 	settings = {
